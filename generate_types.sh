8a15b19e67e31dc8fdc53a8a8298346049644851
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2025-01-19 23:59:12 +0900

    Initialize supabase (#1)
    * update
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ generate_types.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source ./.env
+
+# Start generating TypeScript types for your Supabase schema
+echo "Generating TypeScript types for your Supabase schema..."
+
+# Ensure the PROJECT_REF environment variable is set
+if [ -z $PROJECT_REF ]; then
+  echo "PROJECT_REF environment variable is not set in .env file" >&2
+  exit 1
+fi
+
+# Generate TypeScript types for your Supabase schema
+npx supabase gen types --lang=typescript \
+  --project-id $PROJECT_REF \
+  --schema public > src/types/supabase.ts
+
+# Check if the generation was successful
+if [ $? -eq 0 ]; then
+  echo "TypeScript types generated successfully."
+else
+  echo "Failed to generate TypeScript types." >&2
+  exit 1
+fi
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import { Hono } from 'hono'
 import { supabase } from './lib/supabase';
 
-const app = new Hono()
+type Bindings = {
+  APP_ENV: string;
+  SUPABASE_KEY: string;
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
 app.get("/test", async (c) => {
-  const data = await supabase.from("dev_couples").select("*");
+  const { data } = await supabase(c.env.SUPABASE_KEY).from("dev_couples").select("*");
   console.log(data);
   return c.json(data);
 });
+
+app.get("/env", (c) => {
+  const v = c.env.SUPABASE_KEY;
+  return c.text(v);
+})
 
 export default app
