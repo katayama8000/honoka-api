3f48deb39b787409e7c70efc96e9fbcb1e342c96
katayama8000 <tattu.0310@gmail.com>
2025-01-19 23:57:44 +0900

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/supabase";
 
 const supabaseUrl = "https://jqovxmsueffhddmyqcew.supabase.co";
 
-export const supabase = (apiKey: string) => createClient(supabaseUrl, apiKey)
+export const supabase = (apiKey: string) =>
+    createClient<Database>(supabaseUrl, apiKey);
